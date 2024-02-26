99bf9284eb7681a3068286058ed8001e7bd9ecd8
Katie <pie10101011@gmail.com>
2024-02-25 20:59:39 -0800

    readme

--- lib/seed.ts
+++ lib/seed.ts
@@ -6,7 +6,6 @@ export async function seed() {
       id SERIAL PRIMARY KEY,
       name VARCHAR(255) NOT NULL,
       email VARCHAR(255) UNIQUE NOT NULL,
-      image VARCHAR(255),
       "createdAt" TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
     );
     `

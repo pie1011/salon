63b1f943ed6a1f3bfe1c170499f68d2c4b590aa3
Katie <pie10101011@gmail.com>
2024-02-27 19:11:43 -0800

    starting registration

---
+++ components/register.tsx
@@ -0,0 +1,18 @@
+// "use client";
+import React from "react";
+import { pacifico } from "@/app/fonts";
+
+export default function Hero() {
+  return (
+    <main>
+      <div className="rounded text-center md:text-left m-2 p-8 xl:pr-40 xl:pl-20 bg-gradient-to-b md:bg-gradient-to-r from-white to-white-100/0">
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Harum,
+          molestias exercitationem iste soluta voluptate velit unde, aliquam
+          saepe ea ipsa est hic neque veniam culpa molestiae quo assumenda quam
+          inventore.
+        </p>
+      </div>
+    </main>
+  );
+}

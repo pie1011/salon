369526d85d95953595bdd9fd3b32d3b18171665f
Katie <pie10101011@gmail.com>
2024-03-02 18:01:17 -0800

    minor cleaning

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,6 @@
 import React from "react";
-import Navigation from "@/components/navigation";
 import Hero from "@/components/hero";
 import FrontPage from "@/components/front-page";
-import FooterText from "@/components/footer-text";
-import FooterSticky from "@/components/footer-sticky";
 
 const SalonPage: React.FC = () => {
   return (

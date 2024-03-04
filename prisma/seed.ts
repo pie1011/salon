e02b9f9d187721a69b100285d975db255b9fbc40
Katie <pie10101011@gmail.com>
2024-03-03 16:17:13 -0800

    fix seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,44 +1,7 @@
 import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 async function main() {
-    const alice = await prisma.user.upsert({
-        where: { email: 'alice@prisma.io' },
-        update: {},
-        create: {
-            email: 'alice@prisma.io',
-            name: 'Alice',
-            appointments: {
-                create: {
-                    customer: 'Sally',
-                    service: 'Cut and Color',
-                    confirmed: true,
-                },
-            },
-        },
-    })
-    const bob = await prisma.user.upsert({
-        where: { email: 'bob@prisma.io' },
-        update: {},
-        create: {
-            email: 'bob@prisma.io',
-            name: 'Bob',
-            appointments: {
-                create: [
-                    {
-                        customer: 'Billy',
-                        service: 'Cut and Shave',
-                        confirmed: true,
-                    },
-                    {
-                        customer: 'Jeff',
-                        service: 'Cut',
-                        confirmed: true,
-                    },
-                ],
-            },
-        },
-    })
-    console.log({ alice, bob })
+    console.log('Seeding database...')
 }
 main()
     .then(async () => {

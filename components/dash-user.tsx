8a719b28842762099897b886d9926c7e9cafb8ca
Katie <pie10101011@gmail.com>
2024-03-05 16:36:24 -0800

    updated dash-user

--- components/dash-user.tsx
+++ components/dash-user.tsx
@@ -10,11 +10,13 @@ export default function DashUser() {
   return (
     <main className="shadow-lg bg-white xl:m-10 xl:p-10 m-2 p-2 ">
       {isSignedIn ? (
-        <h3 className="text-2xl font-bold">Hello, {user?.firstName}</h3>
+        <div>
+          <h3 className="text-2xl font-bold">Hello, {user?.firstName}</h3>
+          <p>Email: {primaryEmailAddress}</p>
+        </div>
       ) : (
         <h2 className="text-2xl font-bold">Hello, Guest</h2>
       )}
-      <p>Email: {primaryEmailAddress}</p>
     </main>
   );
 };

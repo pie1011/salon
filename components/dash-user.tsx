76f859ff1cc208a63e19d6c0428c2e4b3a146169
Katie <pie10101011@gmail.com>
2024-03-31 15:13:03 -0700

    user style

--- components/dash-user.tsx
+++ components/dash-user.tsx
@@ -8,11 +8,10 @@ export default function DashUser() {
   const primaryEmailAddress = user?.primaryEmailAddress?.emailAddress
 
   return (
-    <main className="shadow-lg bg-white xl:m-10 xl:p-10 m-2 p-2 ">
+    <main className="xl:m-10 xl:p-10 m-2 p-2 ">
       {isSignedIn ? (
         <div>
-          <h3 className="text-2xl font-bold">Hello, {user?.firstName}</h3>
-          <p>Email: {primaryEmailAddress}</p>
+          <h3 className="text-2xl font-bold">Hello, {user?.firstName} ({primaryEmailAddress})</h3>
         </div>
       ) : (
         <h2 className="text-2xl font-bold">Hello, Guest</h2>
